87e91c31b318a7baacb7345d76d969e0c3d79791
atique24@gmail.com <Powai@Mumbai47>
2020-12-01 13:18:48 +0000

    added drivers

--- base/WebDriverFactory.py
+++ base/WebDriverFactory.py
@@ -6,16 +6,16 @@ class WebDriverFactory():
 
     def get_browser_instance(self):
         if self.browser == "FF":
-            driver = webdriver.Firefox()
+            driver = webdriver.Firefox(executable_path="drivers//geckodriver.exe")
 
         elif self.browser == "Chrome":
-            driver = webdriver.Chrome()
+            driver = webdriver.Chrome(executable_path="drivers//chromedriver.exe")
 
         elif self.browser == "IE":
-            driver = webdriver.Ie()
+            driver = webdriver.Ie(executable_path="drivers//IEDriverServer.exe")
 
         else:
-            driver = webdriver.Chrome()
+            driver = webdriver.Chrome(executable_path="drivers//chromedriver.exe")
 
 
         baseUrl = "http://live.demoguru99.com/index.php/"
