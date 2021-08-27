5a1c8bed44fed44dc45db4f50c01ccef7c4421ba
atique22@gmail.com <atique24@gmail.com>
2021-08-28 00:39:37 +0100

    Refactored Selenium Base class with new methods

---
+++ datafiles/config_browserstack.py
@@ -0,0 +1,16 @@
+bb_url = "https://test_9ALXi0:2d1ZShaTYo9bUN66doVJ@hub-cloud.browserstack.com/wd/hub"
+key = '2d1ZShaTYo9bUN66doVJ'
+localIdentifier = 'local'
+browser_config = {
+      'os_version': '10',
+      'os': 'Windows',
+      'browser': 'firefox',
+      'acceptSslCerts': 'true',
+      'browser_version': 'latest',
+      'name': 'Demo Test', # test name
+      'build': 'browserstack-build-1',
+      'browserstack.local': 'true',
+      'browserstack.localIdentifier': 'local',
+      'resolution' : '2048x1536'
+      }
+
