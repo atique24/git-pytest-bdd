19daebd2b59acd0a6549caab7511d1e054108582
atique24 <atique24@gmail.com>
2021-08-28 01:27:07 +0100

    pytest.ini file added for pytest configuration

---
+++ datafiles/config_browserstack.py
@@ -0,0 +1,16 @@
+bb_url = " Your Browser Stack URL "
+key = 'Your Browserstack Key'
+localIdentifier = 'local'
+browser_config = {
+      'os_version': '10',
+      'os': 'Windows',
+      'browser': 'firefox',
+      'acceptSslCerts': 'true',
+      'browser_version': 'latest',
+      'name': 'Demo Test', # test name
+      'build': 'browserstack-build-1',
+      'browserstack.local': 'true',
+      'browserstack.localIdentifier': 'local',
+      'resolution' : '2048x1536'
+      }
+
