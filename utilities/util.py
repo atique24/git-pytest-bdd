a649e06581d48657ccc1f7b84f19d71faf9ccac7
atique24 <atique24@gmail.com>
2021-08-28 01:25:48 +0100

    Updated the imports

--- utilities/util.py
+++ utilities/util.py
@@ -1,3 +1,5 @@
+import datetime
+
 from utilities.customlogger import custom_logger
 import time
 import random
@@ -17,90 +19,12 @@ class Utilities():
             self.cl.error("Exception occured while Sleep")
 
 
-    def verify_text_contains(self,actualText, expectedText):
-        self.cl.info("Actual text from application URL is :: " + str(actualText))
-        self.cl.info("Expected text from application URL is :: " + str(expectedText))
-
-        if actualText.lower() in expectedText.lower():
-            self.cl.info("### Verfication Passed !!!")
-            return True
-        else:
-            self.cl.info("### Verfication Failed !!!")
-            return False
 
 
-    def verify_text(self,actualText, expectedText):
-        self.cl.info("Actual text from application URL is :: " + str(actualText))
-        self.cl.info("Expected text from application URL is :: " + str(expectedText))
 
-        if actualText.lower() == expectedText.lower():
-            self.cl.info("### Verfication Passed")
-            return True
-        else:
-            self.cl.log.info("### Verfication Failed")
-            return False
-
-    def getAlphaNumeric(self,length,type):
-
-        alpha_num = ""
-
-        if type == "lower":
-            value = string.ascii_lowercase
-
-        elif type == "upper":
-            value = string.ascii_uppercase
-
-        elif type == "digits":
-            value = string.digits
-
-        elif type == "mix":
-            value = string.ascii_letters + string.digits
-
-        elif type == "letters":
-            value = string.ascii_letters
-
-        for i in range(0,length):
-            return alpha_num.join(random.choice(value))
-
-
-    def listcompare(self,expectedList, actualList):
-        try:
-            self.cl.info("Expected List is :: " + str(expectedList))
-            self.cl.info("Actual List is :: " + str(actualList))
-
-            if len(expectedList) == len(actualList):
-                i = 0
-                for i in range(0, len(actualList)):
-                    if expectedList[i] == actualList[i]:
-                        i = i + 1
-                        if i == len(actualList):
-                            return True
-                            self.log.info("Both List Matched")
-
-                    else:
-                        return False
-                        self.log.info("List Does not match")
-                        break
-            else:
-                print("List Length does not match")
-                return False
-
-        except:
-            print("List Length does not match")
-            return False
-
-
-    def verify_values(self,expectedValue,ActualValue):
-        try:
-            if expectedValue == ActualValue:
-                self.cl.info("Matched")
-                return True
-            else:
-                self.cl.info("Values dont match")
-                return False
-        except:
-            self.cl.info("Exception occured.. Values dont match")
-            return False
+    def generate_date_time(self):
+        now = datetime.datetime.now()
+        return now.strftime('%Y-%m-%d %H-%M-%S-%f')
 
 
 
